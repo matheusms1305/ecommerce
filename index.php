2a8abbd498a1a951103b56f4246c030228a05d80
matheusms1305 <matheusmsiqueira98@outlook.com>
2024-04-12 20:56:58 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
-
+session_start();
+// o slim é responsavel pelo endereço (pelas rotas) da minha navegacao
 require_once("vendor/autoload.php");
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Slim\Slim;
+use \Hcode\Model\User;
+
+
 $app = new Slim();
 
 
@@ -16,11 +20,31 @@ $app->get('/', function() {
 
 });
 $app->get('/admin', function() {
-    
+    User::verifyLogin();
+
 	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin/login', function(){
+	$page = new PageAdmin(["header"=>false,"footer"=>false]);
+	$page->setTpl("login");
+
+});
+$app->post('/admin/login', function(){
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+$app->get('admin/logout', function(){
+	User::logout();
+
+	header("Location: /admin/login");
+});
+
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -3,15 +3,19 @@ namespace Hcode;
 //namespace rain pra instanciar a classe tpl
 use Rain\Tpl;
 
-class Page {
+// a classe page vai ser responsavel por criar nossos te
+class Page { 
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>false,
         "data"=>[]
     ];
-    //primeiro a ser executado
+    //(primeiro a ser executado)a funcao construct recebe dois parametros: 1- um array vazio, 2: um arry com uma chave, e vamos fundir os dois mais tarde
     public function __construct($opts = array(), $tpl_dir = "/views/")
     {
+        //no construct eu recebo os dados pelo array $opts, depois passo eles pra valores no meu array options que tem a chave "data" e por fim uso o array_merge pra fundir os dois
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
@@ -22,18 +26,21 @@ class Page {
         Tpl::configure($config);
         
         $this->tpl = new Tpl;
-        
+        //a funcao setData faz os assigns dos dados do meu objeto tpl, pra isso eu passo pra ela o array options, que é o resultado da soma do array $default e do $opts 
         $this->setData($this->options["data"]);
-        //escreve o header html
-        $this->tpl->draw("header");
+        //escreve o header html(ele busca pelo metodo configure, meu arquivo header no diretorio especificado dinamicamente no parametro $config)
+        if($this->options["header"] === true)  $this->tpl->draw("header");
+           
+        
+   
     }
-    //cria nossos 'assigns' 
+     //cria nossos 'assigns' 
     public function setData($data = array()){
         foreach($data as $key => $value){
             $this->tpl->assign($key, $value);
         }
     }
-    //nome da pagina, dados, corpo da pagina
+    //busca o nome do template que sera carregado, passando o nome do arquivo e retorna a pagina
     public function setTpl($name, $data = array(), $returnHTML = false){
         $this->setData($data);
         return $this->tpl->draw($name, $returnHTML);
@@ -46,7 +53,7 @@ class Page {
     //footer do site
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 
 }
